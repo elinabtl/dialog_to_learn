207561a1a9cd4344cbf95d9a7332c161aadfe248
Elina Bartal <ekleinerman@gmail.com>
2017-08-16 08:21:09 +0300

    Fix some problems with regex on emails.

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0,'/usr/local/lib/python3.6/site-packages')
+sys.path.insert(0, '/usr/local/lib/python3.6/site-packages')
 
 import smtplib
 import time
@@ -18,8 +18,7 @@ from difflib import Differ
 
 # Utility to read email from Gmail Using Python
 
-# TODO:
-# 1. If handling an email fails - mark it as unread again.
+# TODO: If handling an email fails - mark it as unread again.
 
 IMAP_SERVER = "imap.gmail.com"
 SMTP_SERVER = "smtp.gmail.com"
@@ -53,38 +52,40 @@ def readClassData():
   # CLASS_DATA.teacher_email = 'aaa'
   # participant = CLASS_DATA.participants.add()
   # participant.real_email = 'bbb'
-  # participant.alias_email = 'ccc'  
+  # participant.alias_email = 'ccc'
 
 # Go over all the unread emails - and send the messages accordingly
 def readEmailFromGmail():
   for classIndex, currentClass in enumerate(CLASS_DATA.classes):
     try:
       mail = imaplib.IMAP4_SSL(IMAP_SERVER)
-      mail.login(currentClass.class_email,currentClass.class_pwd)
+      mail.login(currentClass.class_email, currentClass.class_pwd)
       mail.select('inbox')
 
       type, data = mail.search(None, 'ALL', '(UNSEEN)')
       idList = data[0].split()
 
       # Print the count of all unread messages
-      print("<<<Class: " + currentClass.class_email + " , unread emails: " + str(len(idList)) + ">>>\n")
+      print("<<<Class: " + currentClass.class_email + " , unread emails: " +
+            str(len(idList)) + ">>>\n")
 
       server = smtplib.SMTP(SMTP_SERVER, 587)
       server.starttls()
       server.login(currentClass.class_email, currentClass.class_pwd)
 
       for idx in idList:
-        typ, data = mail.fetch(idx, '(RFC822)' )
+        typ, data = mail.fetch(idx, '(RFC822)')
         for response_part in data:
           if isinstance(response_part, tuple):
             msg = email.message_from_string(response_part[1].decode('utf-8'))
-            emailData = readEmail(msg, classIndex)
+            emailData = readEmail(msg, classIndex, currentClass.class_email)
             if emailData != None:
               toRealEmail, message = composeEmail(emailData)
               server.sendmail(currentClass.class_email, toRealEmail, message)
               print("Sending email to " + toRealEmail + "\n")
             else:
-              print("Email was from unrecognized account: " + msg['from'] + "\n")
+              print("Email was from unrecognized account: " + msg['from'] +
+                    "\n")
 
     except Exception as e:
       print(str(e))
@@ -92,7 +93,7 @@ def readEmailFromGmail():
     finally:
       server.quit()
 
-def readEmail(msg, classIndex):
+def readEmail(msg, classIndex, classEmail):
   emailSubject = msg['subject']
   emailFrom = msg['from']
   emailTo = msg['to']
@@ -100,10 +101,12 @@ def readEmail(msg, classIndex):
   for part in msg.walk():
     if part.get_content_type() == "text/plain":
       emailContent = part.get_payload()
-  lastName, demoEmail = findLastName(email.utils.parseaddr(emailFrom)[1], classIndex)
-  if lastName==None and demoEmail==None:
+  lastName, demoEmail = findLastName(email.utils.parseaddr(emailFrom)[1],
+                                     classIndex)
+  if lastName == None and demoEmail == None:
     return None
-  sendData = parseEmail(emailFrom, emailTo, emailSubject, emailContent, lastName, classIndex)
+  sendData = parseEmail(emailFrom, emailTo, emailSubject, emailContent,
+                        lastName, classIndex, classEmail)
   return EmailData(sendData, demoEmail, classIndex)
 
 def composeEmail(emailData):
@@ -111,20 +114,21 @@ def composeEmail(emailData):
   # Find the real destination email in csv:
   msg['from'] = CLASS_DATA.classes[emailData.classIndex].class_email
   msg['To'] = emailData.sendData.sendToEmail
-  msg['Subject'] = emailData.sendData.subject # couldn't change the from address to contain +name, so added reply to option
+  # couldn't change the from address to contain +name, so added reply to option:
+  msg['Subject'] = emailData.sendData.subject
   msg.add_header('reply-to', emailData.aliasEmail)
   msg.attach(MIMEText(emailData.sendData.content, 'plain'))
   return emailData.sendData.sendToEmail, msg.as_string()
 
 def findLastName(email, classIndex):
   for user in CLASS_DATA.classes[classIndex].participants:
-    if user.real_email==email:
+    if user.real_email == email:
       return user.last_name, user.alias_email
   return None, None
 
 def findRealEmail(demoEmail, classIndex):
   for user in CLASS_DATA.classes[classIndex].participants:
-    if user.alias_email==demoEmail:
+    if user.alias_email == demoEmail:
       return user.real_email
   return None
 
@@ -132,25 +136,37 @@ def appendCensoredToText(input_text, censored_text):
   l1 = input_text.split(' ')
   l2 = censored_text.split(' ')
   dif = list(Differ().compare(l1, l2))
-  return " ".join([i[2:] if i[:1] == '-' or i[:1] == '+' else i[2:] for i in dif])
+  return " ".join([i[2:] if i[:1] == '-' or i[:1] == '+' else
+                  i[2:] for i in dif])
 
-def parseEmail(emailFrom, emailTo, subject, content, lastName, classIndex):
+def parseEmail(emailFrom, emailTo, subject, content, lastName, classIndex,
+               classEmail):
   # profanity filter from: https://pythonhosted.org/profanityfilter/
   regexp = re.compile(PHONE_EMAIL_NAME_REGEX%lastName)
   print(' From : ' + emailFrom)
   print(" To : " + emailTo)
   print(' Subject : ' + subject)
   print(' Content: ' + content)
-  if regexp.search(content) or profanityfilter.is_profane(content) or regexp.search(subject) or profanityfilter.is_profane(subject):
-    print(" Email or phone or last name or profanity in subject or content send to: " + CLASS_DATA.classes[classIndex].teacher_email)
+  reContent = regexp.search(content)
+  if (reContent and reContent.group() != classEmail) or \
+      profanityfilter.is_profane(content) or regexp.search(subject) or \
+      profanityfilter.is_profane(subject):
+    print(" Email or phone or last name or profanity in subject or content" +
+          "send to: " + CLASS_DATA.classes[classIndex].teacher_email)
     censoredSubject = profanityfilter.censor(subject)
     censoredContent = profanityfilter.censor(content)
     censoredSubject = appendCensoredToText(subject, censoredSubject)
     censoredContent = appendCensoredToText(content, censoredContent)
-    filteredSubject = re.sub(PHONE_EMAIL_NAME_REGEX%lastName, r'*CONTENT_PROBLEM*: \1', censoredSubject, flags=re.IGNORECASE)
-    filteredContent = re.sub(PHONE_EMAIL_NAME_REGEX%lastName, r'*CONTENT_PROBLEM*: \1', censoredContent, flags=re.IGNORECASE)
-    filteredSubject = "CONTENT_PROBLEM by " + email.utils.parseaddr(emailFrom)[1] + ": " + filteredSubject
-    return SendData(CLASS_DATA.classes[classIndex].teacher_email, filteredSubject, filteredContent)
+    filteredSubject = re.sub(PHONE_EMAIL_NAME_REGEX%lastName,
+                             r'*CONTENT_PROBLEM*: \1', censoredSubject,
+                             flags=re.IGNORECASE)
+    filteredContent = re.sub(PHONE_EMAIL_NAME_REGEX%lastName,
+                             r'*CONTENT_PROBLEM*: \1', censoredContent,
+                             flags=re.IGNORECASE)
+    filteredSubject = "CONTENT_PROBLEM by {0} : {1}".format(
+      email.utils.parseaddr(emailFrom)[1], filteredSubject)
+    return SendData(CLASS_DATA.classes[classIndex].teacher_email,
+                    filteredSubject, filteredContent)
   else:
     parsedEmail = email.utils.parseaddr(emailTo)[1]
     realEmail = findRealEmail(parsedEmail, classIndex)
